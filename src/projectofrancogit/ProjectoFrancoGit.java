652328159197382d642b0f03666f9346503243dc
fmorba <fgmorbidoni@gmail.com>
2018-07-06 13:31:36 -0300

    Ejercicio Git Plan 111

---
+++ src/projectofrancogit/ProjectoFrancoGit.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectofrancogit;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ProjectoFrancoGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Random numerosAleatorios= new Random();
+       int num;
+       
+       num=numerosAleatorios.nextInt(100);
+       
+        System.out.println("El numero elegido al azar es: "+num);
+       
+    }
+    
+}
